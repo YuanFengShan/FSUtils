272e7e04c59b982b7e2c30c4a6610c4f374fc82c
YuanFengShan <645633369@qq.com>
2017-12-16 17:24:39 +0800

    新增使用BufferedReader来读取文件的内容API

--- app/src/main/java/com/jdboy/fsutils/fsutils/FileUtil.java
+++ app/src/main/java/com/jdboy/fsutils/fsutils/FileUtil.java
@@ -21,6 +21,13 @@ public class FileUtil {
         return Helper.intnace;
     }
 
+    /**
+     * 复制文件
+     *
+     * @param indexFile
+     * @param copyFile
+     * @throws IOException
+     */
     public void copyFile(File indexFile, File copyFile) throws IOException {
         if (!indexFile.exists()) {
             throw new FileNotFoundException();
@@ -28,5 +35,26 @@ public class FileUtil {
         FileInputStream fileInputStream = new FileInputStream(indexFile);
         FileOutputStream fileOutputStream = new FileOutputStream(copyFile);
         IOUtil.getInstance().inputStream2outputStream(fileInputStream, fileOutputStream);
+    }
+
+    /**
+     * 使用BufferedReader来读取文件的内容
+     *
+     * @param filePath 被读取文件的路径
+     * @return
+     * @throws IOException
+     */
+    public String readFileContent(String filePath) throws IOException {
+        StringBuffer stringBuffer = new StringBuffer();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+        //存放读取出来的每行的内容
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuffer.append(line);
+            //可选择性是否换行符
+            stringBuffer.append("\r\n");
+        }
+        bufferedReader.close();
+        return stringBuffer.toString();
     }
 }
