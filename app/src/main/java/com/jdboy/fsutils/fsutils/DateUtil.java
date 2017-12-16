1db3b590a7d206bf1c8e61d1d8d5c4da9baf4d33
YuanFengShan <645633369@qq.com>
2017-12-16 17:29:26 +0800

    新增获取当前时间API

--- app/src/main/java/com/jdboy/fsutils/fsutils/DateUtil.java
+++ app/src/main/java/com/jdboy/fsutils/fsutils/DateUtil.java
@@ -10,30 +10,34 @@ import java.util.Date;
  * 只是简单记录下格式化当前系统时间的年月日小时分钟秒的标识
  * Created by 64563 on 2017/12/11.
  */
-public class DataUtil {
-    private DataUtil() {
+public class DateUtil {
+    private DateUtil() {
 
     }
 
     private static class Helper {
-        private static DataUtil intance = new DataUtil();
+        private static DateUtil intance = new DateUtil();
     }
 
-    public static DataUtil getIntnace() {
+    public static DateUtil getIntnace() {
         return Helper.intance;
     }
 
     /**
      * 获取当前时间
      *
-     * @param context
-     * @return
+     * @return 返回样例(2017年12月16日 17时28分45秒)
      */
-    public String getCurrentData(Context context) {
+    public String getCurrentDate() {
         long l = System.currentTimeMillis();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
         Date curDate = new Date(l);//获取当前时间
         String str = formatter.format(curDate);
         return str;
+    }
+
+    public static void main(String[] args) {
+        String currentDate = DateUtil.getIntnace().getCurrentDate();
+        System.out.println("当前时间:" + currentDate);
     }
 }
